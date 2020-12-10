39f9e191f47f458b6e52a64c3b9ad5fe5943830a
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-11 00:07:06 +0400

    feat: added schema user

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const path = require('path');
 
@@ -7,6 +8,12 @@ const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongoose://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const sendMessageError = (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
