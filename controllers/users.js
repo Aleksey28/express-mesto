fc3138a2baca84db3551aa82cfa4e230d86a0185
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-12 17:04:46 +0400

    feat: added validation for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,38 @@
 const User = require('../models/user');
 
+const ERROR_WRONG_DATA_CODE = 400;
+const ERROR_NOT_FOUND_CODE = 404;
+const ERROR_DEFAULT_CODE = 500;
+
+const ERROR_DEFAULT_MESSAGE = 'Server was broken =(';
+
+const VALIDATION_ERROR_NAME = 'ValidationError';
+
 const getUsers = (req, res) => {
   User.find({})
     .then((data) => {
+      if (data.length <= 0) {
+        res.status(ERROR_NOT_FOUND_CODE).send({ message: 'Список пользователей пуст' });
+        return;
+      }
       res.send(data);
     })
     .catch(() => {
-      res.status(500).send({ message: 'Server was broken =(' });
+      res.status(ERROR_DEFAULT_CODE).send({ message: ERROR_DEFAULT_MESSAGE });
     });
 };
 
 const getUser = (req, res) => {
   User.findById(req.params.id)
     .then((data) => {
+      if (!data) {
+        res.status(ERROR_NOT_FOUND_CODE).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
       res.send(data);
     })
     .catch(() => {
-      res.status(404).send({ message: 'Нет пользователя с таким id' });
+      res.status(ERROR_DEFAULT_CODE).send({ message: ERROR_DEFAULT_MESSAGE });
     });
 };
 
@@ -27,7 +43,12 @@ const createUser = (req, res) => {
       res.send(data);
     })
     .catch((error) => {
-      res.status(404).send({ message: error });
+      console.log(error.name);
+      if (error.name === VALIDATION_ERROR_NAME) {
+        res.status(ERROR_WRONG_DATA_CODE).send({ message: error.message });
+      } else {
+        res.status(ERROR_DEFAULT_CODE).send({ message: ERROR_DEFAULT_MESSAGE });
+      }
     });
 };
 
