986ca27dbe5a64de6a75985c4e0249458405d8b6
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-12 17:09:30 +0400

    feat: moved constants to unique file

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,11 @@
 const User = require('../models/user');
-
-const ERROR_WRONG_DATA_CODE = 400;
-const ERROR_NOT_FOUND_CODE = 404;
-const ERROR_DEFAULT_CODE = 500;
-
-const ERROR_DEFAULT_MESSAGE = 'Server was broken =(';
-
-const VALIDATION_ERROR_NAME = 'ValidationError';
+const {
+  ERROR_WRONG_DATA_CODE,
+  ERROR_NOT_FOUND_CODE,
+  ERROR_DEFAULT_CODE,
+  ERROR_DEFAULT_MESSAGE,
+  VALIDATION_ERROR_NAME,
+} = require('../utils/constants');
 
 const getUsers = (req, res) => {
   User.find({})
