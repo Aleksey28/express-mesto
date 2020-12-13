a0aef09f086403fe5410f1788bb0eb27d627ef53
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-13 16:26:36 +0400

    ref: refactoring sendError

--- utils/functions.js
+++ utils/functions.js
@@ -9,19 +9,21 @@ const {
 function sendError(res, error = undefined, status = undefined) {
   if (error !== undefined && status !== undefined) {
     res.status(status).send({ message: error });
-  } else if (error !== undefined) {
-    let errorCode;
-    switch (error.name) {
-      case VALIDATION_ERROR_NAME:
-        errorCode = ERROR_WRONG_DATA_CODE;
-        break;
-      default:
-        errorCode = ERROR_DEFAULT_MESSAGE;
-    }
-    res.status(errorCode).send({ message: error.message });
-  } else {
+    return;
+  } if (error === undefined) {
     res.status(ERROR_DEFAULT_CODE).send({ message: ERROR_DEFAULT_MESSAGE });
+    return;
   }
+
+  let errorCode;
+  switch (error.name) {
+    case VALIDATION_ERROR_NAME:
+      errorCode = ERROR_WRONG_DATA_CODE;
+      break;
+    default:
+      errorCode = ERROR_DEFAULT_MESSAGE;
+  }
+  res.status(errorCode).send({ message: error.message });
 }
 
 module.exports = { sendError };
